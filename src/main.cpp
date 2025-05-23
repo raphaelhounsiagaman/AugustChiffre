0f04c27a40e16503df0f1ed0822485fad771aba6
raphaelhounsiagaman <rhounsiagaman@gmail.com>
2025-05-23 10:44:39 +0200

    Implemented Encoding

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,6 @@ void decode_with_brute_force();
 int main()
 {
 start:
-
 	std::cout << "Choose an action:\n";
 	std::cout << "1. Encode\n";
 	std::cout << "2. Decode with code\n";
@@ -32,18 +31,45 @@ start:
 		break;
 	}
 	
-	int pause;
-	std::cin >> pause;
+	main();
 	return 0;
 }
 
 void encode()
 {
+restart:
 	std::cout << "What would you like your code to be?\n";
-	std::cout << "(Must be between 0 and 25)\n";
+	std::cout << "(Must be between 1 and 25)\n";
 
 	int code;
 	std::cin >> code;
+	if (code < 1 || code > 25)
+	{
+		std::cerr << "Invalid code. Please try again.\n";
+		goto restart;
+	}
+
+	std::cout << "What text would you like to encode with code: " << code << "?\n";
+
+	std::string text;
+	std::cin.ignore();
+	std::getline(std::cin, text);
+	
+	std::cout << "Encoding Text with code: " << code << " : \n\n" << text << "\n";
+	for (char& c : text)
+	{
+		if (isalpha(c))
+		{
+			char base = islower(c) ? 'a' : 'A';
+			c = (c - base + code) % 26 + base;
+		}
+	}
+
+	std::cout << "\nEncoded Text: \n\n" << text << "\n";
+	std::cin.ignore();
+	std::cin.get();
+	
+
 }
 
 void decode_with_code()
